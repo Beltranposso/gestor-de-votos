027fc8a8cd1bed931048ea5e1cbc73baa31baad8
Beltranposso <148396397+Beltranposso@users.noreply.github.com>
2024-07-12 10:37:09 -0500

    Initial commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+
+export  default function Home (){
+    return(
+    
+          <div className="contenedorHome">
+            
+            <h1>Home</h1>
+    
+          </div> 
+    
+    )
+    }
