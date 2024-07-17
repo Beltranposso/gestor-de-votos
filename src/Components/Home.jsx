01a293d203b7f60d5caf0ecc400d29271a3fe30b
tuusuafiodegithub <tuemailgithub>
2024-07-17 16:42:12 -0500

    se agrego la creacion de plantillas (las preguntas y titulo)

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,48 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Card_Home from './Card_votacion';
 
-
 const Home = () => {
-  
+  const [cards, setCards] = useState([
+    { titulo: "Titulo_1", hora: "22:00", color: "#FFE8A3" },
+    { titulo: "Titulo_2", hora: "13:00", color: "green" },
+    { titulo: "Titulo_2", hora: "13:00", color: "red" },
+    { titulo: "Titulo_2", hora: "13:00", color: "Blue" },
+  ]);
 
+  const addCard = (newCard) => {
+    setCards([...cards, newCard]);
+  };
 
   return (
     <div className="Home">
-      
-      
       <div className="search-bar_Home">
         <div>
-        <input className='input_text_buscar' type="text" placeholder="Buscar" />
-        <span className="filter-icon">A-Z</span>
+          <input className='input_text_buscar' type="text" placeholder="Buscar" />
+          <span className="filter-icon">A-Z</span>
         </div>
-       
-        <button className="create-button   ">Crear +</button>
+        <Link className="create-button" to='/Creacion'>Crear +</Link>
       </div>
       <div className='Content_Card'>
         <h2 className='fs-3'>Encuestas en curso</h2>
-          <div  className='content_Card_2'>
-
-            <Card_Home titulo={"Titulo_1"} hora={"22:00"} color={"#FFE8A3"}></Card_Home>
-            <Card_Home titulo={"Titulo_2"} hora={"13:00"} color={"green"}></Card_Home>
-            <Card_Home titulo={"Titulo_2"} hora={"13:00"} color={"red"}></Card_Home>
-            <Card_Home titulo={"Titulo_2"} hora={"13:00"} color={"Blue"}></Card_Home>
-            <Card_Home titulo={"Titulo_2"} hora={"13:00"} color={"black"}></Card_Home>
-            <Card_Home titulo={"Titulo_2"} hora={"13:00"} color={"grey"}></Card_Home>
-            <Card_Home titulo={"Titulo_2"} hora={"13:00"} color={"purple"}></Card_Home>
-            <Card_Home titulo={"Titulo_2"} hora={"13:00"} color={"Yellow"}></Card_Home>
-  
-
-          
-           
-           
-           
-          </div>
-     
-     
+        <div className='content_Card_2'>
+          {cards.map((card, index) => (
+            <Card_Home key={index} titulo={card.titulo} hora={card.hora} color={card.color} />
+          ))}
+        </div>
       </div>
-  
-      </div>
-    
+    </div>
   );
 };
 
