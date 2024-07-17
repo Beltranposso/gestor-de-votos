3dbc89fa088ba5047c063fcc791b41ff69ba8abe
Beltranposso <148396397+Beltranposso@users.noreply.github.com>
2024-07-17 16:56:15 -0500

    se añadio la funcionalidad del boton de delete

--- src/Components/listUsers.jsx
+++ src/Components/listUsers.jsx
@@ -12,16 +12,19 @@ export default function ListUsers() {
 
   useEffect(() => {
     getUser();
+     
   }, []);
 
   const getUser = async () => {
     const response = await axios.get(URI);
     setUser(response.data);
   };
-
+  
+   
    const deleteUser = async (id) => {
-    await axios.delete(`${URI}${id}`);
-
+     location.reload();
+     await axios.delete(`${URI}${id}`);
+     
    }
 
 
@@ -80,7 +83,7 @@ export default function ListUsers() {
                   <path d="M19 16v6" />
                     </svg>
                 </Link>
-                <button   className=' d-flex align-items-center btn btn-danger' onClick={()=>deleteUser(user.id)}><svg xmlns="http://www.w3.org/2000/svg" width={18} height={18  } viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-trash">
+                <button   className=' d-flex align-items-center btn btn-danger' onClick={()=>deleteUser(user.id)}   ><svg xmlns="http://www.w3.org/2000/svg" width={18} height={18  } viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-trash">
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M4 7l16 0" />
                   <path d="M10 11l0 6" />
